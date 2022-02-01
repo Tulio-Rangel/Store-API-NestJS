564a344962c456e92da83177ed4bcebdc444cdd2
Tulio Rangel <tjrn20@hotmail.com>
2022-02-01 18:01:10 -0500

    move all products controllers to their proper file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,31 +15,11 @@ export class AppController {
     return 'New Endpoint';
   }
 
-  //recibiendo parámetros
-  @Get('products/:productId')
-  getProducts(@Param() params: any) {
-    return `product ${params.productId}`;
-  }
 
-  // otra forma de recibir parametros
-  // @Get('products/:productId')
-  // getProducts2(@Param(productId) productId: string) {
-  //   return `product ${productId}`;
-  // }
 
   //ruta con varios ids
   @Get('categories/:id/products/:productId')
   getCategory(@Param('id') id: string, @Param('productId') productId: string) {
     return `product ${productId} and ${id}`;
-  }
-
-  //Query params
-  @Get('products')
-  getProducts2(
-    @Query('limit') limit: number,
-    @Query('brand') brand: string,
-    @Query('offset') offset = 0, //así se pasa un valor por defecto
-  ) {
-    return `product: limit => ${limit} offset => ${offset} brand => ${brand}`;
   }
 }
