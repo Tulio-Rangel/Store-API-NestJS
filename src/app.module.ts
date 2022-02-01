a3a512404f47d390a1748295b2735cbb6cd35641
Tulio Rangel <tjrn20@hotmail.com>
2022-02-01 18:24:08 -0500

    create endpoints controller for all aplication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
+import { OrdersController } from './controllers/orders.controller';
+import { UsersController } from './controllers/users.controller';
+import { BrandsController } from './controllers/brands.controller';
+import { CustomersController } from './controllers/customers.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [AppController, ProductsController, CategoriesController, OrdersController, UsersController, BrandsController, CustomersController],
   providers: [AppService],
 })
 export class AppModule {}
