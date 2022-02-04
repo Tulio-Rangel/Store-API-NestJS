ecd089f62d6beb4f888de4a7b6a485731fbcaa4c
Tulio Rangel <tjrn20@hotmail.com>
2022-02-03 19:18:26 -0500

    create entity & use it in product.service file

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      desciption: 'Here goes the descriotion',
+      price: 12000,
+      stock: 1,
+      image: 'googleImageHere',
+    },
+  ];
+}
