776ca4754455db93d07f42630404410dfb160a25
Tulio Rangel <tjrn20@hotmail.com>
2022-02-03 19:48:15 -0500

    create methods in services file(have to be verify)

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -3,6 +3,7 @@ import { Product } from 'src/entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private counterId = 1;
   private products: Product[] = [
     {
       id: 1,
@@ -13,4 +14,56 @@ export class ProductsService {
       image: 'googleImageHere',
     },
   ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  update(payload: any, id: number) {
+    const getProductById = this.products.find((item) => item.id === id);
+    this.products[getProductById] = {
+      id: id,
+      ...payload,
+    };
+  }
+
+  update1(payload: any, id: number) {
+    const product = this.findOne(id);
+
+    for (let key in payload) {
+      if (key !== 'id') {
+        product[key] = payload[key];
+      }
+    }
+
+    return product;
+  }
+
+  update2(id: number, payload: any) {
+    const found = this.products.findIndex((item) => item.id === id);
+
+    if (found === -1) throw new Error('Product not found');
+    this.products[found] = {
+      id: id,
+      ...payload,
+    };
+    return {
+      Message: 'Product updated',
+      Updated: this.products[found],
+    };
+  }
 }
