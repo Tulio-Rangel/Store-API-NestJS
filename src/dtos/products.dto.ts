87eafb4c7416455523d8f0f63f53a38f420df33e
Tulio Rangel <tjrn20@hotmail.com>
2022-02-28 15:07:27 -0500

    add validator

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,16 +1,41 @@
+import { IsString, IsNumber, IsUrl, IsNotEmpty } from "class-validator";
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
 //El signo "?" básicamente dice que el atributo es opcional
 export class UpdateProductDto {
+  @IsString()
   readonly name?: string;
+
+  @IsString()
   readonly description?: string;
+
+  @IsNumber()
   readonly price?: number;
+
+  @IsNumber()
   readonly stock?: number;
+
+  @IsUrl()
   readonly image?: string;
 }
