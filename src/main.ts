73411b18fa318ee34bc78d3899e39d64ae4233fa
Tulio Rangel <tjrn20@hotmail.com>
2022-03-02 20:06:40 -0500

    evitando parámetros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:
+        true /**Whitelis hace que todo lo que no definí para la BD en el DTO se borre automaticamente */,
+      /*forbidNonWhitelisted:
+        true Esto lo que hace es que te dice que hay un problema y para, el whitelis simplemente lo ignora,*/
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
